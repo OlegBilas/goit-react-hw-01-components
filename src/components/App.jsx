3ab3d1394c1a7bd76f2b4dd07c3478b8c892db88
OlegBilas <olegbilas@gmail.com>
2023-04-27 16:37:50 +0300

    завдання1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import user from 'data/user';
+import { Profile } from "components/Profile/Profile";
+
+export const App = () => 
+   (<Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />);
