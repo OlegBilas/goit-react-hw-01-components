3ee8d2c79a7a994ffe62e6400f7b07e328fe94a5
OlegBilas <olegbilas@gmail.com>
2023-04-28 10:59:10 +0300

    prop types is defined

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
-import css  from "components/Profile/Profile.module.css";
+import PropTypes from "prop-types";
+import css from "components/Profile/Profile.module.css";
 
 export const Profile = ({ username,
     tag,
@@ -31,3 +32,15 @@ export const Profile = ({ username,
     </li>
   </ul>
 </div>);
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  })
+}
